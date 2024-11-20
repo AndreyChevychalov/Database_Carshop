21a37a06e75b2ad3942325382a453a7c1fe80fb9
AndreyChevychalov <ach1312@yandex.ru>
2024-11-20 08:36:36 +0300

    Изменён код

---
+++ Domain/Client2.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="Client2.cs" company="Чевычалов А.В.">
+// Copyright (c) Чевычалов А.В.. All rights reserved.
+// </copyright>
+
+using System.Collections.Generic;
+using System;
+
+namespace Domain
+{
+    public sealed class Client2 : Person
+    {
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="Client2"/>.
+        /// </summary>
+        /// <param name="name">Имя.</param>
+        public Client2(string name)
+            : base(name)
+        {
+            this.Cars = new HashSet<Car>();
+        }
+
+        public new ISet<Car> Cars { get; }
+
+        public void AddCar(Car car)
+        {
+            if (car == null)
+            {
+                throw new ArgumentNullException(nameof(car));
+            }
+
+            this.Cars.Add(car);
+        }
+    }
+}
---
+++ Domain/Person.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="Person.cs" company="Чевычалов А.В.">
+// Copyright (c) Чевычалов А.В.. All rights reserved.
+// </copyright>
+
+namespace Domain
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Абстрактный класс Person, представляющий человека.
+    /// </summary>
+    public abstract class Person
+    {
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="Person"/>.
+        /// </summary>
+        /// <param name="name">Имя.</param>
+        public Person(string name)
+        {
+            this.PersonId = Guid.Empty;
+            this.Name = name ?? throw new ArgumentNullException(nameof(name));
+        }
+
+        /// <summary>
+        /// Идентификатор.
+        /// </summary>
+        public Guid PersonId { get; }
+
+        /// <summary>
+        /// Имя.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Список автомобилей.
+        /// </summary>
+        public ISet<Car> Cars { get; } = new HashSet<Car>();
+    }
+}
